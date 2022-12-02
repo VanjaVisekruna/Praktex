5461239dbf52c7e49f098d8c9586fdf83ae59cb4
Vanja Visekruna <vanja.visekruna05@gmail.com>
2022-12-02 10:21:43 +0100

    newest version

--- praktex/src/main/java/org/acme/User.java
+++ praktex/src/main/java/org/acme/User.java
@@ -1,2 +1,22 @@
-package org.acme;public class User {
+package org.acme;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.ws.rs.GET;
+
+//@Entity
+@Getter
+@Setter
+public class User {
+    //@Id
+    //@Column(name = "id", nullable = false)
+    int id;
+
+    int Id;
+    String name;
+
 }
